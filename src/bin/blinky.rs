6649c15d2705eb91fcf759e889bb39aa210013e9
daley <daleydeng@gmail.com>
2024-12-03 12:50:49 +0800

    init

---
+++ src/bin/blinky.rs
@@ -0,0 +1,31 @@
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::{
+    delay::Delay,
+    gpio::{Level, Output},
+    prelude::*,
+};
+use esp_println::println;
+
+#[entry]
+fn main() -> ! {
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+
+    println!("Hello world!");
+
+    // Set GPIO7 as an output, and set its state high initially.
+    let mut led = Output::new(peripherals.GPIO8, Level::Low);
+
+    led.set_high();
+
+    // Initialize the Delay peripheral, and use it to toggle the LED state in a
+    // loop.
+    let delay = Delay::new();
+
+    loop {
+        led.toggle();
+        delay.delay_millis(500u32);
+    }
+}
